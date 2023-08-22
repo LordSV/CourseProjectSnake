3b36caf141affd9b189fa95b11488efa311da58f
Ян <lsvjhm@gmail.com>
2023-08-22 18:44:52 +0300

    W5L2

--- Client/MultiplayerSnake/Assets/Scripts/Snake.cs
+++ Client/MultiplayerSnake/Assets/Scripts/Snake.cs
@@ -6,6 +6,7 @@ public class Snake : MonoBehaviour
     [SerializeField] private Transform _directionPoint;
     [SerializeField] private float _speed = 2f;
     [SerializeField] private float _rotateSpeed = 90f;
+    private Vector3 _targetDirection = Vector3.zero;
 
     private void Update()
     {
@@ -15,15 +16,17 @@ public class Snake : MonoBehaviour
 
     private void Rotate()
     {
-        float diffY = _directionPoint.eulerAngles.y - _head.eulerAngles.y;
+        Quaternion targetRotation = Quaternion.LookRotation(_targetDirection);
+        _head.rotation = Quaternion.RotateTowards(_head.rotation, targetRotation, Time.deltaTime * _rotateSpeed);
+        //float diffY = _directionPoint.eulerAngles.y - _head.eulerAngles.y;
 
-        if (diffY > 180f) diffY = (diffY - 180f) * -1f;
-        else if (diffY < -180f) diffY = (diffY +180f) * -1f;
+        //if (diffY > 180f) diffY = (diffY - 180f) * -1f;
+        //else if (diffY < -180f) diffY = (diffY +180f) * -1f;
 
-        float maxAngle = Time.deltaTime * _rotateSpeed;
-        float rotateY = Mathf.Clamp(diffY, -maxAngle, maxAngle);
+        //float maxAngle = Time.deltaTime * _rotateSpeed;
+        //float rotateY = Mathf.Clamp(diffY, -maxAngle, maxAngle);
 
-        _head.Rotate(0, rotateY, 0);
+        //_head.Rotate(0, rotateY, 0);
     }
 
     private void Move()
@@ -33,6 +36,7 @@ public class Snake : MonoBehaviour
 
     public void LookAt(Vector3 cursorPosition)
     {
-        _directionPoint.LookAt(cursorPosition);
+        _targetDirection = cursorPosition - _head.position;
+        //_directionPoint.LookAt(cursorPosition);
     }
 }
