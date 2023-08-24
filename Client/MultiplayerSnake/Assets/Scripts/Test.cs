8ce990000ac5e7d1268bc3b6c4c3843a18c4ec1f
Ян <lsvjhm@gmail.com>
2023-08-24 20:11:29 +0300

    W5L4

--- Client/MultiplayerSnake/Assets/Scripts/Test.cs
+++ Client/MultiplayerSnake/Assets/Scripts/Test.cs
@@ -3,8 +3,7 @@ using UnityEngine;
 public class Test : MonoBehaviour
 {
     [SerializeField] private int _detailsCount;
-    [SerializeField] private Controller _controllerPrefab;
-    [SerializeField] private Snake _snakePrefab;
+
     private Controller _controller;
     private Snake _snake;
     void Update()
@@ -19,10 +18,7 @@ public class Test : MonoBehaviour
             {
                 Destroy(_controller.gameObject);
             }
-            _snake = Instantiate(_snakePrefab);
-            _snake.Init(_detailsCount);
-            _controller = Instantiate(_controllerPrefab);
-            _controller.Init(_snake);
+
         }
     }
 }
